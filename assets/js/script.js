124b21dfeebfb8b589cf946b7cb015963349affe
OnlyMeHere <bennett45.j.m@gmail.com>
2022-07-02 11:52:33 -0700

    add notation to js

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+// city search returns current and future weather
+
+// that city is then added to search history
+
+// that city's name, the date, an icon of weather conditions
+// temperature, humdity, wind speed, UV index.
+
+// the UV index is color coded: favorable/moderate/severe
+
+
+// future conditions are presented as 5 day forcast that
+// displayed with an icon representation of conditions, 
+// temperature, wind speed and humidity
+
+
+
+// clicking on the city in search history diplays the
+// current weather and 5 day forecast for that city
