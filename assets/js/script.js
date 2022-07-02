214ff729abab902b4f7fb803f02bcb4265da2cae
OnlyMeHere <bennett45.j.m@gmail.com>
2022-07-02 14:51:01 -0700

    add API link to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
+var city = "San Francisco"
+var country = "US"
+
+
+
+var reqUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=f1854a42b65c3a76fa9e1197b0e5cd1d&units=imperial';
 
 // city search returns current and future weather
+
+
+fetch(reqUrl)
+    // --- calls out to the API for data
+    .then(function(response) {
+        console.log("response", response)
+        return response.json();
+    // ---receives the data and parses it from a string to and object
+    })
+    .then(function (data) {
+        console.log("data", data)
+        // ----receives the object and passes it to be rendered on the page
+
+    });
 
 // that city is then added to search history
 
